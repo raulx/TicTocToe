ea0319cb5a913b18b676d9865b2851338b3e5497
Raulx103 <raulx103@gmail.com>
2023-06-26 18:27:59 +0530

    third commit

--- public/index.js
+++ public/index.js
@@ -1,1 +1,52 @@
+const crossBox = document.getElementById('cross-box');
+const circleBox = document.getElementById('circle-box');
+const dynamicCircle = document.getElementById('dynamic-circle');
+const dynamicCross = document.getElementById('dynamic-cross');
 
+
+let playerChoice;
+function userChoice(event){
+    const choosen = event.target.id
+    if(choosen==='dynamic-cross'){
+        playerChoice = 'x';
+        console.log('x');
+        // change selected cross-box styles
+        crossBox.style.backgroundColor = '#f9c831';
+        dynamicCross.style.setProperty('--dynamic-cross','#ff4d4d')
+
+        //change not selected circle-box styles
+
+        circleBox.style.background = '#ff4d4d';
+        dynamicCircle.style.setProperty('--dynamic-circle-background-color','#f9c831')
+        dynamicCircle.style.setProperty('--dynamic-circle-psuedo-color','#ff4d4d')
+
+
+    }
+    else if(choosen==='dynamic-circle'){
+        playerChoice = 'o'
+        console.log('o')
+        // change selected circle-box styles
+        circleBox.style.backgroundColor = '#f9c831';
+        dynamicCircle.style.setProperty('--dynamic-circle-background-color','#ff4d4d')
+        dynamicCircle.style.setProperty('--dynamic-circle-psuedo-color','#f9c831')
+
+
+        //change not selected cross-box styles
+        crossBox.style.backgroundColor = '#ff4d4d';
+        dynamicCross.style.setProperty('--dynamic-cross','#f9c831')
+    }
+}
+
+// crossBox.addEventListener('click',()=>{
+//     choosen = 'x';
+//     let crossColor = choosen==='x'?'#ff4d4d':'#f9c831';
+//     dynamicCross.style.setProperty('--dynamic-cross',crossColor)
+// })
+// circleBox.addEventListener('click',()=>{
+//     choosen = 'o';
+//     let circleColor = choosen==='o'?'#ff4d4d':'#f9c831';
+//     dynamicCircle.style.setProperty('--dynamic-circle',circleColor)
+// })
+
+crossBox.addEventListener('click',userChoice);
+circleBox.addEventListener('click',userChoice);
